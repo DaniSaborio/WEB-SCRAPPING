b8073e20e925effcb2e4ed30f1cf363712f2bfbc
DaniSaborio <danisabo8713@gmail.com>
2025-07-23 00:07:14 -0600

    DYNAMIC SCRAPPING listo

--- scrapper/scrapper_dynamic.py
+++ scrapper/scrapper_dynamic.py
@@ -1,7 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver import chrome # Mantengo esto ya que lo tenías en tus imports originales
 from webdriver_manager.chrome import ChromeDriverManager
 import time
 import os
@@ -20,6 +19,7 @@ STEAM_PASSWORD = os.getenv('STEAM_PASSWORD')
 chrome_options = Options()
 
 # Inicializar WebDriver
+
 service = Service(ChromeDriverManager().install())
 driver = webdriver.Chrome(service=service, options=chrome_options)
 
@@ -30,7 +30,7 @@ try:
     time.sleep(2)
 
     # --- Localizar campos de texto y contraseña ---
-    username_field = driver.find_element("css selector", 'input[type="text"]')
+    username_field = driver.find_element('css selector', 'input[type="text"]')
     password_field = driver.find_element("css selector", 'input[type="password"]')
 
     username_field.send_keys(STEAM_USERNAME)
@@ -41,14 +41,20 @@ try:
     sign_in_button.click()
 
     # Espera después de intentar iniciar sesión para observar el resultado
-    time.sleep(10)
+    time.sleep(25)
+    #ENTRAR A DESCUENTOS
+    more_button = driver.find_element('xpath', '//div[contains(@class, "responsive_content_dive") and contains(text(), "Más")]')
+    more_button.click()
+    #FILTRAR POR JUEGOS RPG Y ACCCION 
 
+    time.sleep(10)
 except Exception as e:
     print(f"Ocurrió un error durante el intento de inicio de sesión: {e}")
-    # Puedes añadir aquí capturas de pantalla o logs para depuración
-    # driver.save_screenshot("error_screenshot.png")
 
 finally:
     # Asegura que el navegador se cierre
     if driver:
         driver.quit()
+
+        #//*[@id="home_specialoffers"]/span/a/div/div
+        #//*[@id="responsive_page_template_content"]/div[3]/div[1]/div/div/div/div[2]/div/form/div[4]/button
